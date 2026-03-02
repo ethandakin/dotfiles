67691dbaa035853563739e84f2f9aad1cf0c59e7
ethan <ethandakin@outlook.com.au>
2026-03-02 22:15:29 +1100

    feat: refactored niri and dms setup

--- flake.nix
+++ flake.nix
@@ -8,16 +8,6 @@
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    niri = {
-      url = "github:sodiboo/niri-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };    
-  
-    dms = {
-      url = "github:AvengeMedia/DankMaterialShell/stable";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }:
@@ -27,18 +17,12 @@
       specialArgs = { inherit inputs; };
       modules = [
         ./hosts/${host}
-        inputs.niri.nixosModules.niri
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.ethan = import ./home/home.nix;
           home-manager.extraSpecialArgs = { inherit inputs; };
-          home-manager.sharedModules = [
-            inputs.niri.homeModules.niri
-            inputs.dms.homeModules.dank-material-shell
-            inputs.dms.homeModules.niri
-          ];
         }
       ];
     };
---
+++ modules/system/dms.nix
@@ -0,0 +1,16 @@
+{ ... }: {
+  programs.dms-shell = {
+    enable = true;
+
+    systemd = {
+      enable = true;             # Systemd service for auto-start
+      restartIfChanged = true;   # Auto-restart dms.service when dms-shell changes
+    };
+    
+    # Core features
+    enableSystemMonitoring = true;     # System monitoring widgets (dgop)
+    enableAudioWavelength = true;      # Audio visualizer (cava)
+    enableCalendarEvents = true;       # Calendar integration (khal)
+    enableClipboardPaste = true;       # Pasting from the clipboard history (wtype)
+  };
+}
