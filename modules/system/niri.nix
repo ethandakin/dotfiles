798f0e96122d403398ad4f2e007c1dd0f555644e
ethan <ethandakin@outlook.com.au>
2026-03-02 22:25:13 +1100

    feat: added greetd

--- modules/system/niri.nix
+++ modules/system/niri.nix
@@ -1,6 +1,16 @@
 { pkgs, ... }: {
   programs.niri.enable = true;
 
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "niri-session";
+        user = "ethan";
+      };
+    };
+  };
+
   xdg.portal = {
     enable = true;
     extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
