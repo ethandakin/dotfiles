61a8fdf1575ee22a7efdaf4a70680c72b2a4691c
ethan <ethandakin@outlook.com.au>
2026-03-02 21:09:09 +1100

    feat: added laptop

---
+++ hosts/prawn/default.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }: {
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/system/audio.nix
+    ../../modules/system/boot.nix
+    ../../modules/system/locale.nix
+    ../../modules/system/networking.nix
+    ../../modules/system/niri.nix
+    ../../modules/system/nix.nix
+  ];
+
+  networking.hostName = "prawn";
+  
+  services.tlp = {
+    enable = true;
+  };
+  
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+  services.blueman.enable = true;  
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  services.fwupd.enable = true;
+  services.libinput.enable = true;
+  services.thermald.enable = true;  
+}
