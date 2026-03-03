6a092dd984c61763887adcbd79ddfb53a19d0bb7
ethan <ethandakin@outlook.com.au>
2026-03-04 01:46:40 +1100

    fix: skip niri overlay

--- modules/system/niri.nix
+++ modules/system/niri.nix
@@ -2,7 +2,9 @@
   programs.niri.enable = true;
   programs.niri.package = pkgs.niri-unstable;
 
-  programs.niri.settings.hotkey-overlay.skip-at-startup = true;
+  programs.niri.settings = {
+    hotkey-overlay.skip-at-startup = true;
+  };
 
   # avoid conflict with dms
   systemd.user.services.niri-flake-polkit.enable = false;
