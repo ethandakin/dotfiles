3b685a6eae656c25ad6f7a83f0f7be383bf3e44a
ethan <ethandakin@outlook.com.au>
2026-03-03 18:16:45 +1100

    fix: added nixos-bgrt and grub skip

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,6 +1,7 @@
 { ... }: {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.timeout = 0;
 
   boot.consoleLogLevel = 0;
   boot.kernelParams = [ "quiet" "splash" "udev.log_level=0" ];
@@ -8,7 +9,8 @@
 
   boot.plymouth = {
     enable = true;
-    theme = "spinner";
+    themePackages = [ pkgs.nixos-plymouth-theme ];
+    theme = "nixos-bgrt";
   };
 
   boot.initrd.systemd.enable = true;  
