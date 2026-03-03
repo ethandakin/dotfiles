4027feef3f2873b39aa7eff5413f1218eb46fe15
ethan <ethandakin@outlook.com.au>
2026-03-03 19:07:25 +1100

    fix: test niri cache and nix optimizations

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -2,12 +2,22 @@
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
     auto-optimise-store = true;
+
+    max-jobs = "auto";
+    cores = 0;
+
+    trusted-users = [ "root" "ethan" ];
   };
 
   nix.gc = {
     automatic = true;
     dates = "weekly";
     options = "--delete-older-than 30d";
+  };
+
+  nix.optimise = {
+    automatic = true;
+    dates = [ "weekly" ];
   };
 
   nixpkgs.config.allowUnfree = true;
