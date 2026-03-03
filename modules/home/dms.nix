f7204abd74435f0d1dee2737ca516e8205454585
ethan <ethandakin@outlook.com.au>
2026-03-03 18:33:35 +1100

    refactor: change dms and niri to flakes

---
+++ modules/home/dms.nix
@@ -0,0 +1,15 @@
+{ ... }: {
+  programs.dank-material-shell = {
+    enable = true;
+
+    niri = {
+      enableSpawn = true;    # start with niri, fixes grey flash
+      enableKeybinds = true;
+    };
+
+    enableSystemMonitoring = true;
+    enableDynamicTheming = true;
+    enableAudioWavelength = true;
+    enableClipboardPaste = true;
+  };
+}
--- modules/system/niri.nix
+++ modules/system/niri.nix
@@ -1,23 +1,14 @@
-{ pkgs, ... }: {
+{ ... }: {
   programs.niri.enable = true;
 
   services.greetd = {
     enable = true;
-    settings = {
-      default_session = {
-        command = "niri-session";
-        user = "ethan";
-      };
+    settings.default_session = {
+      command = "niri-session";
+      user = "ethan";
     };
   };
 
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
-    config.common.default = "*";
-  };
-
-  environment.systemPackages = with pkgs; [
-    xwayland-satellite
-  ];
+  # avoid conflict with dms
+  systemd.user.services.niri-flake-polkit.enable = false;
 }
