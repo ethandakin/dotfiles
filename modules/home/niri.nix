5f32800812598bccb28015b9ff09a3708b104211
ethandakin <ethand@outlook.com.au>
2026-03-05 21:54:29 +1100

    fix: add noctalia on startup

--- modules/home/niri.nix
+++ modules/home/niri.nix
@@ -1,5 +1,8 @@
 { ... }: {
   programs.niri.settings = {
-    hotkey-overlay.skip-at-startup = true;
+    hotkey-overlay.skip-at-startup = false;
+    spawn-at-startup = [
+      { command = [ "noctalia-shell" ]; }
+    ];    
   };  
 }
