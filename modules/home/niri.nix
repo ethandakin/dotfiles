b3aa6fb8a1b4cfc743694c988c623405f2a4db3d
ethandakin <ethand@outlook.com.au>
2026-03-08 01:28:17 +1100

    fix: niri binds attribute

--- modules/home/niri.nix
+++ modules/home/niri.nix
@@ -2,26 +2,26 @@
   programs.niri.settings = {
     hotkey-overlay.skip-at-startup = false;
 
-    binds = {
-      "Mod+Shift+Slash".action = { show-hotkey-overlay = true };
+    binds = with config.lib.niri.actions; {
+      "Mod+Shift+Slash".action = {show-hotkey-overlay = true};
 
       "Mod+T".action.spawn = "wezterm";
       "Mod+T".hotkey-overlay.title = "Open a Terminal: wezterm";
 
-      # # Overview
-      # "Mod+O" = { repeat = false; action = toggle-overview; };
+      # Overview
+      "Mod+O" = { repeat = false; action = toggle-overview; };
 
-      # # Window management
-      # "Mod+Q" = { repeat = false; action = close-window; };
+      # Window management
+      "Mod+Q" = { repeat = false; action = close-window; };
 
-      # "Mod+Left".action  = focus-column-left;
-      # "Mod+Down".action  = focus-window-down;
-      # "Mod+Up".action    = focus-window-up;
-      # "Mod+Right".action = focus-column-right;
-      # "Mod+H".action     = focus-column-left;
-      # "Mod+J".action     = focus-window-down;
-      # "Mod+K".action     = focus-window-up;
-      # "Mod+L".action     = focus-column-right;
+      "Mod+Left".action  = focus-column-left;
+      "Mod+Down".action  = focus-window-down;
+      "Mod+Up".action    = focus-window-up;
+      "Mod+Right".action = focus-column-right;
+      "Mod+H".action     = focus-column-left;
+      "Mod+J".action     = focus-window-down;
+      "Mod+K".action     = focus-window-up;
+      "Mod+L".action     = focus-column-right;
     };
   };  
 }
