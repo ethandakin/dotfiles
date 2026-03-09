9acc830fe0c115c496afd2519842b73120f875da
ethandakin <ethand@outlook.com.au>
2026-03-10 00:59:46 +1100

    fix: bitwarden signing

--- home/home.nix
+++ home/home.nix
@@ -10,18 +10,42 @@
     bitwarden-desktop
   ];
 
-  programs.wezterm.enable = true;
+  programs.wezterm = {
+    enable = true;
+  };
   programs.firefox.enable = true;
+  stylix.targets.firefox.enable = false;
   programs.vscode.enable = true;
+
+  home.file.".ssh/allowed_signers".text = ''
+    ethand@outlook.com.au ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDkZzuL3uE9EWO6ZZP57hjMefwQh72riuxG3m/QGRgsP
+  '';
+
   programs.git = {
     enable = true;
-    userName = "ethandakin";
-    userEmail = "ethand@outlook.com.au";
+    signing = {
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDkZzuL3uE9EWO6ZZP57hjMefwQh72riuxG3m/QGRgsP";
+      signByDefault = true;
+    };
+    settings = {
+      user.name = "ethandakin";
+      user.email = "ethand@outlook.com.au";
+      
+      gpg = {
+        format = "ssh";
+        ssh.allowedSignersFile = "/home/${config.home.username}/.ssh/allowed_signers";
+      };
+    };
   };
 
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
+    matchBlocks."*" = {
+      extraOptions = {
+        IdentityAgent = "/home/${config.home.username}/.bitwarden-ssh-agent.sock";
+      };
+    };
   };
 
   home.sessionVariables = {
