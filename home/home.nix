29c861f1ed3a3057c78bc7ff15336b88ef47869a
Ethan <54067724+ethandakin@users.noreply.github.com>
2026-03-20 15:33:59 +1100

    feat: remove unnecessary stuff in home

--- home/home.nix
+++ home/home.nix
@@ -1,20 +1,15 @@
 { pkgs, inputs, config, ... }: {
   imports = [
-    ./programs/niri
     ./programs/firefox
     ./programs/git
     ./programs/ssh
     ./programs/vicinae
-    ./programs/noctalia
   ];
 
   home.packages = with pkgs; [
     nil
     nixfmt
     bitwarden-desktop
-    kdePackages.qt6ct
-    gtk3
-    gtk4
   ];
 
   programs.alacritty.enable = true;
@@ -24,26 +19,6 @@
   programs.fastfetch.enable = true;
 
   services.tailscale-systray.enable = true;
-
-  gtk = {
-    enable = true;
-    gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
-    gtk4.extraConfig.gtk-application-prefer-dark-theme = true;
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome-themes-extra;
-    };
-  };
-
-  qt = {
-    enable = true;
-    platformTheme.name = "gtk4";
-    style.name = "adwaita-dark";
-  };
-
-  dconf.settings."org/gnome/desktop/interface" = {
-    color-scheme = "prefer-dark";
-  };
 
   home.stateVersion = "26.05";
 }
