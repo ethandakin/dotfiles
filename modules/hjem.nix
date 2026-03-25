38373e686e11cfcb1031745fb139abacdeae3ca3
ethandakin <ethand@outlook.com.au>
2026-03-25 19:58:24 +1100

    fix: noctalia settings permissions

--- modules/hjem.nix
+++ modules/hjem.nix
@@ -5,8 +5,23 @@
     directory = "/home/ethan";
     files = {
       ".config/niri/config.kdl".source = ../home/niri/config.kdl;
-      ".config/noctalia/colors.json".source = ../home/noctalia/colors.json;
-      ".config/noctalia/settings.json".source = ../home/noctalia/settings.json;
+      # copies instead of symlinks
+      ".config/niri/noctalia.kdl" = {
+        source = ../home/niri/noctalia.kdl;
+        type = "copy";
+        permissions = "644";
+      };
+      ".config/noctalia/colors.json" = {
+        source = ../home/noctalia/colors.json;
+        type = "copy";
+        permissions = "644";
+      };
+      ".config/noctalia/settings.json" = {
+        source = ../home/noctalia/settings.json;
+        type = "copy";
+        permissions = "644";
+      };
     };
   };
 }
+
